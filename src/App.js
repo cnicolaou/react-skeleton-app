c983816ff38a923e6e90c82cd4982273edbcc738
Constantine Nicolaou <cnicolaou@mobiquityinc.com>
2019-03-11 11:21:33 +0100

    Install and use react-card-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { UserCard } from 'react-ui-cards';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <UserCard
+          cardClass='float'
+          header='https://i.imgur.com/w5tX1Pn.jpg'
+          avatar='https://i.imgur.com/uDYejhJ.jpg'
+          name='Justin Case'
+          positionName='Software Developer'
+          stats={[
+            {
+              name: 'followers',
+              value: 21
+            },
+            {
+              name: 'following',
+              value: 37
+            },
+            {
+              name: 'posts',
+              value: 117
+            }
+          ]}
+        />
+        <UserCard
+          cardClass='float'
+          header='https://i.imgur.com/w5tX1Pn.jpg'
+          avatar='https://i.imgur.com/uDYejhJ.jpg'
+          name='Justin Case'
+          positionName='Software Developer'
+          stats={[
+            {
+              name: 'followers',
+              value: 21
+            },
+            {
+              name: 'following',
+              value: 37
+            },
+            {
+              name: 'posts',
+              value: 117
+            }
+          ]}
+        />
+        <UserCard
+          cardClass='float'
+          header='https://i.imgur.com/w5tX1Pn.jpg'
+          avatar='https://i.imgur.com/uDYejhJ.jpg'
+          name='Justin Case'
+          positionName='Software Developer'
+          stats={[
+            {
+              name: 'followers',
+              value: 21
+            },
+            {
+              name: 'following',
+              value: 37
+            },
+            {
+              name: 'posts',
+              value: 117
+            }
+          ]}
+        />
       </div>
     );
   }
